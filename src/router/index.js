8565cee136c3b17acce346c4e4ec7adf52d625a6
Sjoedy <joedyxyv@gmail.com>
2020-12-31 18:08:54 +0700

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -11,14 +11,14 @@ import RegisterSuccess from "@/views/Layout/CRUD/RegisterSuccess";
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-    meta: {
-      middleware: [Middlewares.auth]
-    }
-  },
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home,
+  //   meta: {
+  //     middleware: [Middlewares.auth]
+  //   }
+  // },
   {
     path:'/admin-dashboard',
     name:'Dashboard',
@@ -37,7 +37,7 @@ const routes = [
 
   },
   {
-    path:'/user-register',
+    path:'/',
     name:'UserRegister',
     component:Register,
     meta: {
