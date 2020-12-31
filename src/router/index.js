b393712df5555e2c51a79bbe306186063edb56b7
Sjoedy <joedyxyv@gmail.com>
2020-12-31 18:20:03 +0700

    fix env

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
 import RegisterInfo from "@/views/Layout/RegisterInfo";
 import Register from "@/views/Layout/Register";
 import Login from "@/views/Layout/login"
